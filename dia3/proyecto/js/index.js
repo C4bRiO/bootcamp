7ee9d20743f2917ff33fa905d5937f097b579fa1
C4bRiO <everleiva21@gmail.com>
2018-03-13 20:01:10 -0400

    updated html

---
+++ dia3/proyecto/js/index.js
@@ -0,0 +1,18 @@
+function enviarDatos(){
+  var nombre = document.getElementById("nombre").value;
+
+  var descripcion = document.getElementById("descripcion").value;
+
+  var direccion = document.getElementById("direccion").value;
+
+
+  alert(nombre+" "+descripcion+" "+direccion);
+
+  /*console.log(nombre);
+
+
+
+
+
+  console.log(document.getElementsByClassName("form-group"));*/
+};
